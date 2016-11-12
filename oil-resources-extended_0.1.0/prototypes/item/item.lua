ed392a337cb59a06cc30f2e5e77013c321febea8
Loom <looom@gmx.de>
2016-11-12 22:13:55 +0100

    more coal items

--- oil-resources-extended_0.1.0/prototypes/item/item.lua
+++ oil-resources-extended_0.1.0/prototypes/item/item.lua
@@ -2,13 +2,43 @@ data:extend(
 {
   {
     type = "item",
+    name = "coal-dust",
+    icon = "__oil-resources-extended__/graphics/icons/coal-dust.png",
+    flags = {"goes-to-main-inventory"},
+    fuel_value = "8MJ",
+    subgroup = "raw-material",
+    order = "b[coal-dust]",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "briquette",
+    icon = "__oil-resources-extended__/graphics/icons/briquette.png",
+    flags = {"goes-to-main-inventory"},
+    fuel_value = "12MJ",
+    subgroup = "raw-material",
+    order = "b[briquette]",
+    stack_size = 50
+  },
+  {
+    type = "item",
     name = "coke",
     icon = "__oil-resources-extended__/graphics/icons/coke.png",
     flags = {"goes-to-main-inventory"},
-    fuel_value = "18MJ",
+    fuel_value = "14MJ",
     subgroup = "raw-material",
     order = "b[coke]",
     stack_size = 50
-  }
+  },
+  {
+    type = "item",
+    name = "coke-dust",
+    icon = "__oil-resources-extended__/graphics/icons/coke-dust.png",
+    flags = {"goes-to-main-inventory"},
+    fuel_value = "14MJ",
+    subgroup = "raw-material",
+    order = "b[coke-dust]",
+    stack_size = 50
+  },
 
 })
