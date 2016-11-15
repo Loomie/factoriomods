76303b1cfeaa2269eb2229de6f74f386b99d08d4
Boehmel <ericboehlke@web.de>
2016-11-15 21:09:31 +0100

    proof of concept: powerline-conrete now places medium electric pole

--- oil-resources-extended_0.1.0/control.lua
+++ oil-resources-extended_0.1.0/control.lua
@@ -1,76 +1,13 @@
-require ("util")
 
-script.on_event({defines.events.on_built_entity,}, function(event) On_Built(event) end)
-script.on_event({defines.events.on_robot_built_entity,}, function(event) On_Built(event) end)
-script.on_event({defines.events.on_preplayer_mined_item,}, function(event) On_Remove(event) end)
-script.on_event({defines.events.on_robot_pre_mined,}, function(event) On_Remove(event) end)
-script.on_event({defines.events.on_entity_died,}, function(event) On_Death(event) end)
-	
----------------------------------------------
-function On_Built(event)
-    local entity = event.created_entity
+-- Event: Spieler hat entity gebaut
+script.on_event(defines.events.on_player_built_tile, function(event)
+  placed_something(event.positions, event.player_index)
+end)
 
-	--- powerline-concrete built
-	if tile and tile.name == "powerline-concrete" then
-    game.show_message_dialog{text={"placed powerline"}}
-		local surface = tile.surface
-		local force = tile.force
-		local position = tile.position		   
-		local powerline_concrete = tile
-		local pole_name = "medium-electric-pole"  		
-		
-		local create_hidden_pole = surface.create_entity({name = pole_name, position = position, force = force})
-		
-		create_hidden_pole.minable = false
-		create_hidden_pole.destructible = false 
-		
-		group_entities(cantor(position.x,position.y), { powerline-concrete, create_hidden_pole })	  
-  end
-end
-
----------------------------------------------
-function On_Remove(event)
-	
-	--- Bio Solar Farm has been removed
-	local entity = event.entity
-   	if entity and entity.name == "bi_bio_Solar_Farm" then
-		local pos_hash = cantor(entity.position.x,entity.position.y)
-        local entity_group = getGroup_entities(pos_hash)
-        if entity_group then
-            for ix, vx in ipairs(entity_group) do
-                if vx == entity then
-                    --vx.destroy()
-                else
-                    vx.destroy()
-                end
-            end
-        end
-        ungroup_entities(pos_hash)
+function placed_something(pos, idx)
+	--if e.name == "powerline-concrete" then
+    for index,value in pairs(pos) do
+      game.players[idx].surface.create_entity({name = "medium-electric-pole", position = pos[index], force = game.players[idx].force})
     end
-end  
-  
----------------------------------------------
-function On_Death(event)
-	
-		--- Bio Solar Farm has been destroyed
-	local entity = event.entity
-   	if entity and entity.name == "bi_bio_Solar_Farm" then
-		local pos_hash = cantor(entity.position.x,entity.position.y)
-        local entity_group = getGroup_entities(pos_hash)
-        if entity_group then
-            for ix, vx in ipairs(entity_group) do
-                if vx == entity then
-                    --vx.destroy()
-                else
-                    vx.destroy()
-                end
-            end
-        end
-        ungroup_entities(pos_hash)
-	end
+	--end
 end
-
-
-
-
-
