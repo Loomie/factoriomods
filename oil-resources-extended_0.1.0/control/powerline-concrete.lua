4cd69d2c2b2bbca056fbfced284f3422bb79414c
Boehmel <ericboehlke@web.de>
2016-12-04 21:33:28 +0100

    powerline concrete will destroy its power pole when mined

--- oil-resources-extended_0.1.0/control/powerline-concrete.lua
+++ oil-resources-extended_0.1.0/control/powerline-concrete.lua
@@ -1,12 +1,35 @@
--- Event: Spieler hat entity gebaut
+require ("util")
+
+-- Event: tile gebaut
 script.on_event(defines.events.on_player_built_tile, function(event)
   placed_tile(event.positions, event.player_index)
 end)
 
+-- Event: tile entfernt
+script.on_event(defines.events.on_player_mined_tile, function(event)
+  mined_tile(event.positions)
+end)
+
 function placed_tile(pos, idx)
-  for index,value in pairs(pos) do  
-    if game.players[idx].surface.get_tile(pos[index]).name == "powerline-concrete" then  
-        game.players[idx].surface.create_entity({name = "powerline-concrete-pole", position = pos[index], force = game.players[idx].force})
+  local force = game.players[idx].force
+  
+  for index,value in pairs(pos) do
+    local tile = game.surfaces[1].get_tile(pos[index])
+    
+    if tile.name == "powerline-concrete" then
+      local pole = game.surfaces[1].create_entity({name = "powerline-concrete-pole", position = pos[index], force = force})      
     end
   end
+end
+
+function mined_tile(pos)
+  
+  for index,value in pairs(pos) do  
+    local poles = game.surfaces[1].find_entities_filtered{area = {{pos[index].x-0.1, pos[index].y-0.1}, {pos[index].x+0.1, pos[index].y+0.1}}, name= "powerline-concrete-pole"}
+    if poles ~= nil then
+      for index,value in pairs(poles) do
+          poles[index].destroy()
+      end
+    end
+  end  
 end
--- oil-resources-extended_0.1.0/control/wood.lua
+++ oil-resources-extended_0.1.0/control/wood.lua
@@ -1,8 +1,11 @@
 -- Event: Chunk generated
 script.on_event(defines.events.on_chunk_generated, function(event)
-  place_wood(event.area, event.surface)
+  place_wood(event.area)
 end)
 
-function place_wood(ar, sfc)
-      local entities = game.surfaces[1].find_entities_filtered{area = ar, type= "tree"}
+function place_wood(ar)
+      local tree_entities = game.surfaces[1].find_entities_filtered{area = ar, type= "tree"}
+--      if tree_entities ~= nil then
+--        game.print(ar.left_top.x)
+--      end
 end
